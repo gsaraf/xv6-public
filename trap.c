fa04dd581103e1a109b8df110532fe60cbdd4600
Gahl Saraf <gahl@fb.com>
2018-05-12 16:43:20 +0300

    Add quanta definition and use in trap.c

--- trap.c
+++ trap.c
@@ -100,11 +100,17 @@ trap(struct trapframe *tf)
   if(myproc() && myproc()->killed && (tf->cs&3) == DPL_USER)
     exit();
 
-  // Force process to give up CPU on clock tick.
+  // Force process to give up CPU every QUANTA clock ticks.
   // If interrupts were on while locks held, would need to check nlock.
   if(myproc() && myproc()->state == RUNNING &&
-     tf->trapno == T_IRQ0+IRQ_TIMER)
-    yield();
+     tf->trapno == T_IRQ0+IRQ_TIMER) {
+    acquire(&tickslock);
+    uint xticks = ticks;
+    release(&tickslock);
+    if (xticks - myproc()->laststatechangetime > QUANTA) {
+      yield();
+    }
+  }
 
   // Check if the process has been killed since we yielded
   if(myproc() && myproc()->killed && (tf->cs&3) == DPL_USER)
